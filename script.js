e2869708daa66bf8291ef374cb2ff69073f065a3
Jhon Leoni <jhonyalmeidax@gmail.com>
2023-04-08 19:14:19 -0400

    fix

--- script.js
+++ script.js
@@ -1,55 +1,37 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var inputAno = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (inputAno.value.length == 0 || Number(inputAno.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } else {
-        var inputGender = document.getElementsByName('radiogen')
-        var idade = ano - Number(inputAno.value)
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (inputGender[0].checked) {
-            gênero = 'masculino'
-            if (idade >= 0 && idade < 12 ){
-                // CRIANÇA
-                img.setAttribute('src', 'menino.png')
-            }
-            else if (idade < 23){
-                // JOVEM
-                img.setAttribute('src', 'homem.png')
-            }
-            else if (idade < 60){
-                // ADULTO
-                img.setAttribute('src', 'homem.png')
-            }
-            else{
-                // IDOSO
-                img.setAttribute('src', 'velho.png')
-            }
-        } else if(inputGender[1].checked){
-            gênero = 'feminino'
-            if (idade >= 0 && idade < 12 ){
-                // CRIANÇA
-                img.setAttribute('src', 'menina.png')
-            }
-            else if (idade < 23){
-                // JOVEM
-                img.setAttribute('src', 'mulher.png')
-            }
-            else if (idade < 60){
-                // ADULTA
-                img.setAttribute('src', 'mulher.png')
-            }
-            else{
-                // IDOSA
-                img.setAttribute('src', 'velha.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Gênero ${gênero} com ${idade} anos de idade.`
-        res.appendChild(img)
+function contar() {
+let inic = document.getElementById('inic')
+let fim = document.getElementById('fim')
+let pula = document.getElementById('pula')
+let res = document.getElementById('res')
+
+
+if(inic.value.length == 0 || fim.value.length == 0 || pula.value.length == 0) {
+    res.innerHTML = 'Impossível contar!'
+    window.alert('[ERRO] campos vazios!')
+    
+} else {
+    res.innerHTML = 'Contando... <br>'
+    let i = Number(inic.value)
+    let f = Number(fim.value)
+    let p = Number(pula.value)
+    if (p <= 0) {
+        window.alert('Número inválido! Contando considerando como 1 ')
     }
-} 
+
+    if (i < f) {
+        // CONTAGEM CRESCENTE
+    for(let c = i; c <= f; c += p) {
+      res.innerHTML += ` ${c} \u{1F449}`
+    }
+      res.innerHTML += "FIM! `\u{1F3C1}`"
+    }
+    else {
+        //CONTAGEM  REGRESSIVA
+        for(let c = i; c >= f; c -=p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+            res.innerHTML += "FIM! `\u{1F3C1}`"
+    
+        }
+}
+}
